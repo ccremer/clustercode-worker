4fc0b7a67e1a2a939e699f66d787337b34ece65c
Chris <github.account@chrigel.net>
2018-11-09 23:17:37 +0100

    go fmt

--- cancel.go
+++ cancel.go
@@ -1,64 +1,62 @@
 package main
 
 import (
-    "github.com/streadway/amqp"
-    "os"
-    "strings"
-    "testing"
+	"github.com/streadway/amqp"
+	"os"
+	"strings"
+	"testing"
 )
 
 func failOnError(err error, msg string) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-    TestMain(nil)
+	TestMain(nil)
 }
 
 func TestMain(m *testing.M) {
 
-    conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-    failOnError(err, "Failed to connect to RabbitMQ")
-    defer conn.Close()
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
 
-    ch, err := conn.Channel()
-    failOnError(err, "Failed to open a channel")
-    defer ch.Close()
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
 
+	err = ch.ExchangeDeclare(
+		"task-cancelled", // name
+		"fanout",         // type
+		true,             // durable
+		false,            // auto-deleted
+		false,            // internal
+		false,            // no-wait
+		nil,              // arguments
+	)
+	failOnError(err, "Failed to declare a exchange")
 
-    err = ch.ExchangeDeclare(
-        "task-cancelled",   // name
-        "fanout", // type
-        true,     // durable
-        false,    // auto-deleted
-        false,    // internal
-        false,    // no-wait
-        nil,      // arguments
-    )
-    failOnError(err, "Failed to declare a exchange")
-
-    body := bodyFrom(os.Args)
-    err = ch.Publish(
-        "task-cancelled", // exchange
-        "",     // routing key
-        false,  // mandatory
-        false,  // immediate
-        amqp.Publishing{
-            ContentType: "application/json",
-            Body:        []byte(body),
-        })
-    failOnError(err, "Failed to publish a message")
+	body := bodyFrom(os.Args)
+	err = ch.Publish(
+		"task-cancelled", // exchange
+		"",               // routing key
+		false,            // mandatory
+		false,            // immediate
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
 }
 
-
 func bodyFrom(args []string) string {
-    var s string
-    if (len(args) < 2) || os.Args[1] == "" {
-        s = "hello"
-    } else {
-        s = strings.Join(args[1:], " ")
-    }
-    return s
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "hello"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+	return s
 }
--- main.go
+++ main.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-    "github.com/aellwein/slf4go"
-    _ "github.com/aellwein/slf4go-native-adaptor"
-    "github.com/ccremer/clustercode-worker/compute"
-    "github.com/ccremer/clustercode-worker/health"
-    "github.com/ccremer/clustercode-worker/messaging"
-    "github.com/ccremer/clustercode-worker/util"
-    "github.com/micro/go-config"
-    "github.com/micro/go-config/source/env"
-    "github.com/micro/go-config/source/file"
+	"github.com/aellwein/slf4go"
+	_ "github.com/aellwein/slf4go-native-adaptor"
+	"github.com/ccremer/clustercode-worker/compute"
+	"github.com/ccremer/clustercode-worker/health"
+	"github.com/ccremer/clustercode-worker/messaging"
+	"github.com/ccremer/clustercode-worker/util"
+	"github.com/micro/go-config"
+	"github.com/micro/go-config/source/env"
+	"github.com/micro/go-config/source/file"
 )
 
 var log slf4go.Logger
 
 func main() {
-    log = slf4go.GetLogger("main")
+	log = slf4go.GetLogger("main")
 
-    log.Infof("Loading configuration...")
-    config.Load(
-        file.NewSource(file.WithPath("defaults.yaml")),
-        file.NewSource(file.WithPath("config.yaml")),
-        env.NewSource(env.WithStrippedPrefix("CC")),
-    )
+	log.Infof("Loading configuration...")
+	config.Load(
+		file.NewSource(file.WithPath("defaults.yaml")),
+		file.NewSource(file.WithPath("config.yaml")),
+		env.NewSource(env.WithStrippedPrefix("CC")),
+	)
 
-    log.SetLevel(util.StringToLogLevel(config.Get("log", "level").String("info")))
-    health.Init()
-    compute.Init()
-    messaging.Init()
+	log.SetLevel(util.StringToLogLevel(config.Get("log", "level").String("info")))
+	health.Init()
+	compute.Init()
+	messaging.Init()
 
-    computeRole := "compute"
-    shovelRole := "shovel"
+	computeRole := "compute"
+	shovelRole := "shovel"
 
-    messaging.Connect()
+	messaging.Connect()
 
-    role := config.Get("role").String("compute")
-    if role == computeRole {
-        compute.Start()
-    } else if role == shovelRole {
-        panic("Not implemented yet")
-    } else {
-        util.PanicWithMessage("You need to specify this worker's role using CC_ROLE to one of %s",
-            computeRole, shovelRole)
-    }
+	role := config.Get("role").String("compute")
+	if role == computeRole {
+		compute.Start()
+	} else if role == shovelRole {
+		panic("Not implemented yet")
+	} else {
+		util.PanicWithMessage("You need to specify this worker's role using CC_ROLE to one of %s",
+			computeRole, shovelRole)
+	}
 
-    health.StartServer()
+	health.StartServer()
 
-    forever := make(chan bool)
-    log.Infof("Startup complete")
-    <-forever
+	forever := make(chan bool)
+	log.Infof("Startup complete")
+	<-forever
 }
